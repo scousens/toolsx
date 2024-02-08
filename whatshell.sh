32e78f2ff15786687b3a8ff984a3b7d1d922ba38
Scott Cousens <scousens@nasuni.com>
2024-02-08 11:25:17 -0500

    whatshell - echo what we are doing so we can see from output what we did and what works

--- whatshell.sh
+++ whatshell.sh
@@ -1,11 +1,11 @@
 echo
 echo what shell am I running?
-echo '---- #1'
+echo '---- #1 -- $0'
 echo "$0"
-echo '---- #2'
+echo '---- #2 -- /proc/$$/exe'
 readlink /proc/$$/exe
-echo '---- #3'
+echo '---- #3 -- $SHELL'
 echo "$SHELL"
-echo '---- #4'
+echo '---- #4 -- pid info ps -p $$'
 ps -p $$
 echo '---------'

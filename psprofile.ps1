735c429b6e2b0ed1c404e4c2f40451b1b222b320
Scott Cousens <scousens@nasuni.com>
2024-03-15 08:16:05 -0400

    psprofile add From-UnixEpoc

--- psprofile.ps1
+++ psprofile.ps1
@@ -5,6 +5,10 @@ $colors = $host.privatedata
 $colors.ErrorForegroundColor = 'Cyan'
 set-alias -name ve -value ~/git/venv.py39/bin/activate.ps1
 
+function From-UnixEpoc($epoc) {
+	([System.DateTimeOffset]::FromUnixTimeMilliSeconds($epoc)).DateTime
+}
+
 function hookup {
   Connect-VIServer -Server vcenter-automation.ops.nasuni.net -User scousens@vsphere.local -Password bWTP4yhgc7aB2B-zCq2f
 }

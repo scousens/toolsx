920ffc47ee8885444deb92f678c65861c159c384
Scott Cousens <scousens@nasuni.com>
2025-05-05 16:34:25 -0400

    tail-latest get correct attrib for directory, use PWD and select-object

--- tail-latest.ps1
+++ tail-latest.ps1
@@ -1,10 +1,10 @@
 # tail 'all-logs' in latest test_execution directory
 if ((split-path $PWD.path -leaf) -eq "logs") {
-	$logsdir = '.'
+	$logsdir = $PWD
 } else {
-	$logsdir = 'logs'
+	$logsdir = Join-Path $PWD 'logs'
 }
-$last_log_dir = get-childitem (join-path $logsdir 'test_execution_*') | select -last 1
-$logfile = Join-Path $last_log_dir.name "all-logs.log"
+$last_log_dir = Get-ChildItem (Join-Path $logsdir 'test_execution_2*') | Select-Object -last 1
+$logfile = Join-Path $last_log_dir.fullname "all-logs.log"
 Write-Host -Fore green "Following $logfile"
 Get-Content -Path $logfile -wait
